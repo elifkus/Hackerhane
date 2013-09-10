8aa90e373856e7705dedf77ab154fe0a0e18b07e
Elif T. Kus <elifkus@gmail.com>
2013-09-11 00:28:33 +0300

    Added logging settings

--- src/hackerhane/settings.py
+++ src/hackerhane/settings.py
@@ -174,20 +174,50 @@ SOCIALACCOUNT_PROVIDERS = \
 # the site admins on every HTTP 500 error.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'logfile': {
+            'level':'INFO',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/django.log',
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        '': {
+            'handlers': ['logfile',],
+            'level': 'INFO',
+        },
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['logfile'],
+            'propagate': False,
         },
     }
 }
