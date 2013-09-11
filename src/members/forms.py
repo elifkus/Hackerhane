b9f7a1d6712371220ed9ff840099279a28dc1f4f
Elif T. Kus <elifkus@gmail.com>
2013-09-12 00:09:40 +0300

    Made email field editable when email signup is used

--- src/members/forms.py
+++ src/members/forms.py
@@ -8,8 +8,13 @@ from common.forms import ReadOnlyField
 class SignupForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs['readonly'] = True
+        
+        if not self.initial:
+            self.email = forms.CharField(max_length=256, required=True)
+        else:
+            self.fields['email'].widget.attrs['readonly'] = True
             
+                
     full_name = forms.CharField(max_length=64)
     email = ReadOnlyField()
     cell_phone_number = forms.CharField(max_length=16)
