dc38d27ce96650b14d28450031661978abec5fdb
Elif T. Kus <elifkus@gmail.com>
2013-10-06 16:24:14 +0300

    Added django-jquery

--- src/accounting/urls.py
+++ src/accounting/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls import url, patterns
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 from accounting.models import Transaction
-from accounting.views import JSONListView
+from accounting.views import JSONListView, grid_handler, grid_config
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('',
@@ -23,4 +24,7 @@ urlpatterns = patterns('',
         )), 
         name = 'transaction-list-json'
     ),
+    url(r'^2$', TemplateView.as_view(template_name="accounting/transaction_list2.html")),
+    url(r'^examplegrid/$', grid_handler, name='grid_handler'),
+    url(r'^examplegrid/cfg/$', grid_config, name='grid_config'),
 )
--- src/accounting/views.py
+++ src/accounting/views.py
@@ -1,7 +1,9 @@
 import json
 from django.http import HttpResponse
 from django.views.generic.list import BaseListView
-
+from jqgrid import JqGrid
+from accounting.models import Transaction
+from django.core.urlresolvers import reverse_lazy
 
 class JSONResponseMixin(object):
     """
@@ -26,5 +28,24 @@ class JSONListView(JSONResponseMixin, BaseListView):
     def render_to_response(self, context, **response_kwargs):  
         object_list= [item.as_dict() for item in list(context['object_list'] )]         
         return self.render_to_json_response(object_list, **response_kwargs)
+
     
-    
+class TransactionGrid(JqGrid):
+    model = Transaction # could also be a queryset
+    fields = ['id', 'payment_media', 'type', 'note', 'amount', 'realized_date'] # optional 
+    url = reverse_lazy('grid_handler')
+    caption = 'İşlemler' # optional
+    colmodel_overrides = {
+        'id': { 'editable': False, 'width':10 },
+    }
+
+
+def grid_handler(request):
+    # handles pagination, sorting and searching
+    grid = TransactionGrid()
+    return HttpResponse(grid.get_json(request), mimetype="application/json")
+
+def grid_config(request):
+    # build a config suitable to pass to jqgrid constructor   
+    grid = TransactionGrid()
+    return HttpResponse(grid.get_config(), mimetype="application/json")
