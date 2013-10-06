d53ec1b4ef54f7ae60e9e1baf213b09ac833bf21
Elif T. Kus <elifkus@gmail.com>
2013-10-06 17:50:46 +0300

    Transaction list views decorated with login_required

--- src/accounting/urls.py
+++ src/accounting/urls.py
@@ -11,6 +11,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', login_required(TemplateView.as_view(template_name="accounting/transaction_list.html")), name='transaction-list'),
-    url(r'^transaction-grid/$', grid_handler, name='grid_handler'),
-    url(r'^transaction-grid/cfg/$', grid_config, name='grid_config'),
+    url(r'^transaction-grid/$', login_required(grid_handler), name='grid_handler'),
+    url(r'^transaction-grid/cfg/$', login_required(grid_config), name='grid_config'),
 )
