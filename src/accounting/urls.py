74bd966d3a82df364b6fad2ac8f1752f98a5ef8d
Elif T. Kus <elifkus@gmail.com>
2013-10-06 16:36:06 +0300

    Code cleaning

--- src/accounting/urls.py
+++ src/accounting/urls.py
@@ -5,26 +5,12 @@ Created on Sep 12, 2013
 '''
 from django.conf.urls import url, patterns
 from django.contrib.auth.decorators import login_required
-from django.views.generic.list import ListView
-from accounting.models import Transaction
-from accounting.views import JSONListView, grid_handler, grid_config
+from accounting.views import grid_handler, grid_config
 from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('',
-    url(r'^$', login_required(ListView.as_view(
-        model=Transaction,
-        queryset=Transaction.objects.order_by("-realized_date"),
-        )), 
-        name = 'transaction-list'
-    ),
-    url(r'^json$', login_required(JSONListView.as_view(
-        model=Transaction,
-        queryset=Transaction.objects.order_by("-realized_date"),
-        )), 
-        name = 'transaction-list-json'
-    ),
-    url(r'^2$', TemplateView.as_view(template_name="accounting/transaction_list2.html")),
-    url(r'^examplegrid/$', grid_handler, name='grid_handler'),
-    url(r'^examplegrid/cfg/$', grid_config, name='grid_config'),
+    url(r'^$', login_required(TemplateView.as_view(template_name="accounting/transaction_list.html")), name='transaction-list'),
+    url(r'^transaction-grid/$', grid_handler, name='grid_handler'),
+    url(r'^transaction-grid/cfg/$', grid_config, name='grid_config'),
 )
--- src/accounting/views.py
+++ src/accounting/views.py
@@ -1,33 +1,7 @@
-import json
 from django.http import HttpResponse
-from django.views.generic.list import BaseListView
 from jqgrid import JqGrid
 from accounting.models import Transaction
 from django.core.urlresolvers import reverse_lazy
-
-class JSONResponseMixin(object):
-    """
-    A mixin that can be used to render a JSON response.
-    """
-    def render_to_json_response(self, context, **response_kwargs):
-        """
-        Returns a JSON response, transforming 'context' to make the payload.
-        """
-        return HttpResponse(
-            self.convert_to_json(context),
-            content_type='application/json',
-            **response_kwargs
-        )
-
-    def convert_to_json(self, context):
-        return json.dumps(context)
-
-
-class JSONListView(JSONResponseMixin, BaseListView):
-    
-    def render_to_response(self, context, **response_kwargs):  
-        object_list= [item.as_dict() for item in list(context['object_list'] )]         
-        return self.render_to_json_response(object_list, **response_kwargs)
 
     
 class TransactionGrid(JqGrid):
