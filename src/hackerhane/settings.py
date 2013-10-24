b462011ec958afa4978e25ad10ba59bc691df842
Elif T. Kus <elifkus@gmail.com>
2013-10-24 23:56:26 +0300

    REmoved logging config from settings file

--- src/hackerhane/settings.py
+++ src/hackerhane/settings.py
@@ -172,58 +172,6 @@ SOCIALACCOUNT_PROVIDERS = \
           'AUTH_PARAMS': { 'access_type': 'online' } }
     }
     
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'logfile': {
-            'level':'INFO',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/usr/local/virtualenvs/hs-members-server/logs/django.log',
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        '': {
-            'handlers': ['logfile',],
-            'level': 'INFO',
-        },
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['logfile'],
-            'propagate': False,
-        },
-    }
-}
 
 LOGIN_REDIRECT_URL="/"
 
